23cbbf7d01a4b1bf8d144491a4f7627e26579de5
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2019-09-04 12:38:19 -0300

    Cena 1 montada
    * Foi criado o scriptableObject para as (enum de direções e imagem)

---
+++ Drag-drop-cards/Assets/Scripts/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    //This is my scriptableObject
+    public CardData cardData;
+
+    public Image artworkImage;
+    private string playerAction;
+
+    //OnValidate vai mostrar o objeto montado assim que o script card for atruibuido no slot
+    private void OnValidate()
+    {
+        artworkImage.sprite = cardData.artwork;
+        playerAction = cardData.playerAction.ToString();
+    }
+}
---
+++ Drag-drop-cards/Assets/Scripts/CardData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Card", menuName ="Card")]
+public class CardData : ScriptableObject
+{
+    public PlayerActions playerAction;
+    public Sprite artwork;
+}
