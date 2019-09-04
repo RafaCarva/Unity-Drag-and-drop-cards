d263756552cbe3c160c23905d225b7ff01a2c172
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2019-09-04 16:44:00 -0300

    Adicionado "Drag" na carta
    * Readme atualizado
    * Iniciada a implementação do Drag em Card.cs

--- Drag-drop-cards/Assets/Scripts/Card.cs
+++ Drag-drop-cards/Assets/Scripts/Card.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class Card : MonoBehaviour
+public class Card : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     //This is my scriptableObject
     public CardData cardData;
@@ -16,5 +17,22 @@ public class Card : MonoBehaviour
     {
         artworkImage.sprite = cardData.artwork;
         playerAction = cardData.playerAction.ToString();
+    }
+
+    //Esse método é a implementação da interface IBeginDragHandler
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Drag-and-drop-cards
 Projeto de estudos de drag and drop 
+
+## Dados gerais
+
+Versão do Unity 3D: 2018.4.8f1 (LTS)
+
+## Tutoriais de referência
+ScriptableObject<br/>
+https://www.youtube.com/watch?v=aPXvoWVabPY<br/>
+<br/>
+Enum<br/>
+https://www.youtube.com/watch?v=q0e55ihIMNI&list=PLZhNP5qJ2IA04ah1qbuCKVTmL1Fsr6qio&index=14<br/>
+<br/>
+Drag and Drop<br/>
+https://www.youtube.com/watch?v=bMuYUOIAdnc<br/>
+https://www.youtube.com/watch?v=P66SSOzCqFU<br/>
+<br/>
